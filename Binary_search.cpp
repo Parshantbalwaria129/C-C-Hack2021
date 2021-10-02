9b78d0dc783b57765ea7d69a711e2ffbe26722d5
parshant <home.alexa.parshant@gmail.com>
2021-10-02 03:30:10 -0400

    deleted all comments

--- Binary_search.cpp
+++ Binary_search.cpp
@@ -1,33 +1,7 @@
 // #include<iostream>
-// using namespace std;
-
-// int main(){
-// 	int dataa[] = {10,20,30,40,50,60,70,80,90,100,110,120}; //Array	
-// 	int loc;  										//Location of element
-// 	int item = 60;									//Item to search
-// 	int beg = 0,end = 11, mid = ((beg+end)/2);		//Step 1, BEG = LB, END = UB, MID = INT((BEG+END)/2) 
-// 	while(beg<=end){								//Step 2, BEG <= END
-// 		if (dataa[mid] == item){					//step 2, DATA[MID] = ITEM
-// 			break;
-// 		}
-// 		if(item<dataa[mid]){						//Step 3, ITEM < DATA[MID]
-// 			end = mid-1;							//		  END = MID - 1
-// 		}
-// 		else{										//        ELSE
-// 			beg = mid+1;}							//		  BEG = MID + 1
-// 		mid = int((beg+end)/2);						//Step 4, MID = INT((BEG + END)/2)
-// 	}
-// 	if (dataa[mid] == item){						//Step 5, IF DATA[MID] = ITEM
-// 		loc = mid;									//  	  LOC = MID
-// 		cout<<"Index of item : "<<loc;
-// 	}
-// 	else{											// 		  ELSE
-// 		cout<<"Item not found";						//		  LOC = NULL
-// 	}
-// 	return 0;
-// }
 
 #include <iostream>
+#include<math.h>
 using namespace std;
 
 int main()
