27f674cb8692b9318d7d66d6b6047a4a64f292aa
Aakash <92243341+AakashAttree@users.noreply.github.com>
2021-10-10 16:34:31 +0530

    Update Traversing_linear_array.cpp

--- Traversing_linear_array.cpp
+++ Traversing_linear_array.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int la[10] = {11,12,13,14,15,16,17,18,19,20}; //LA
-    //LB = 0
-	//UB = 9
-	int k = 0;   //K
+    int la[10] = {11,12,13,14,15,16,17,18,19,20};
+   
+	int k = 0;   
     while (k <= 9){
-		cout<<la[k]; //Process
+		cout<<la[k]; 
 		k = k+1;
 	}
 	return 0;
