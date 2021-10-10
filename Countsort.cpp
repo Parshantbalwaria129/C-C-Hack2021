4bd61b449fe83c457548fae64dd0be82d3a4e944
adhilroshan <adhilchra@gmail.com>
2021-10-10 16:58:34 +0530

    Update Countsort.cpp

--- Countsort.cpp
+++ Countsort.cpp
@@ -1,42 +1,48 @@
-// Counting sort which takes negative numbers as well
-#include <algorithm>
-#include <iostream>
-#include <vector>
+// C++ Program for count sort
+#include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
+#define RANGE 255
 
-void countSort(vector<int>& arr)
+void countSort(char arr[])
 {
-	int max = *max_element(arr.begin(), arr.end());
-	int min = *min_element(arr.begin(), arr.end());
-	int range = max - min + 1;
-
-	vector<int> count(range), output(arr.size());
-	for (int i = 0; i < arr.size(); i++)
-		count[arr[i] - min]++;
-
-	for (int i = 1; i < count.size(); i++)
-		count[i] += count[i - 1];
-
-	for (int i = arr.size() - 1; i >= 0; i--) {
-		output[count[arr[i] - min] - 1] = arr[i];
-		count[arr[i] - min]--;
-	}
-
-	for (int i = 0; i < arr.size(); i++)
-		arr[i] = output[i];
+    // The output character array
+    // that will have sorted arr
+    char output[strlen(arr)];
+ 
+    // Create a count array to store count of individual
+    // characters and initialize count array as 0
+    int count[RANGE + 1], i;
+    memset(count, 0, sizeof(count));
+ 
+    // Store count of each character
+    for (i = 0; arr[i]; ++i)
+        ++count[arr[i]];
+ 
+    // Change count[i] so that count[i] now contains actual
+    // position of this character in output array
+    for (i = 1; i <= RANGE; ++i)
+        count[i] += count[i - 1];
+ 
+    // Build the output character array
+    for (i = 0; arr[i]; ++i) {
+        output[count[arr[i]] - 1] = arr[i];
+        --count[arr[i]];
+    }
+ 
+    // Copy the output array to arr, so that arr now
+    // contains sorted characters
+    for (i = 0; arr[i]; ++i)
+        arr[i] = output[i];
 }
-
-void printArray(vector<int>& arr)
-{
-	for (int i = 0; i < arr.size(); i++)
-		cout << arr[i] << " ";
-	cout << "\n";
-}
-
+ 
+// Driver  code
 int main()
 {
-	vector<int> arr = { -5, -10, 0, -3, 8, 5, -1, 10 };
-	countSort(arr);
-	printArray(arr);
-	return 0;
+    char arr[] = "geeksforgeeks";
+ 
+    countSort(arr);
+ 
+    cout << "Sorted character array is " << arr;
+    return 0;
 }
