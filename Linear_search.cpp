35c74f7e103287457aeaa06ebdd68fa384a317de
Tanishk Batheja <67550749+tanishkbatheja22@users.noreply.github.com>
2021-10-11 10:07:45 +0530

    Update Linear_search.cpp

--- Linear_search.cpp
+++ Linear_search.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	int la[] = {10,20,30,40,50,60,70,80,90}; 	// Array
 	int n = 9;									// Total number of elements in array
-	int element = 50;							// Element ot be search
+	int element = 50;							// Element to be search
 	int loc = 4;								// Location of element
 	cout<<n<<endl;
 	la[n] = element;
